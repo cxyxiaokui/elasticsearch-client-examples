cce7f68a613ab3c83330ee3c242f4834d91194cb
lijk <lijk@missfresh.cn>
2021-07-08 15:18:28 +0800

    添加DSL 查询常用操作示例程序

---
+++ src/test/java/cn/zhuoqianmingyue/es/index/RestHighLevelClientDslQueryApiTest.java
@@ -0,0 +1,18 @@
+package cn.zhuoqianmingyue.es.index;
+
+import cn.zhuoqianmingyue.es.Application;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * RestHightLevelClientQueryApiTest
+ *
+ * @author lijk
+ * @date 2021/07/07
+ **/
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class RestHighLevelClientQueryApiTest {
+    
+}
