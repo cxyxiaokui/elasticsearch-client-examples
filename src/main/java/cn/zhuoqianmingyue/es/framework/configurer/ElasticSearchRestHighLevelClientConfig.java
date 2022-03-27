ef00d91f13592d812c3e3a5cbc6d5e176ff45d3c
zhuoqianmingyue <ljk126wy@126.com>
2022-03-27 10:52:13 +0800

    新增高级客户端配置访问权限的示例代码

--- src/main/java/cn/zhuoqianmingyue/es/framework/configurer/ElasticSearchRestHighLevelClientConfig.java
+++ src/main/java/cn/zhuoqianmingyue/es/framework/configurer/ElasticSearchRestHighLevelClientConfig.java
@@ -1,7 +1,14 @@
 package cn.zhuoqianmingyue.es.framework.configurer;
 
+import cn.hutool.core.util.StrUtil;
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -19,6 +26,33 @@ public class ElasticSearchRestHighLevelClientConfig {
 
     @Value("${spring.elasticsearch.nodes:127.0.0.1:9200}")
     private String nodes;
+
+    /**
+     * 设置 ElasticSearch 带密码配置
+     * @return
+     */
+//    @Bean
+//    public RestHighLevelClient highLevelClient() {
+//
+//        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+//        //测试环境es无需配置用户名密码  线上环境阿里es需配置用户名密码
+//        String userName = "";
+//        String password = "";
+//        if(StrUtil.isNotEmpty(userName) && StrUtil.isNotEmpty(password)){
+//            credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(userName, password));
+//        }
+//
+//        RestClientBuilder builder = RestClient.builder(new HttpHost("", 2323))
+//                .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+//                    @Override
+//                    public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
+//                        return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+//                    }
+//                });
+//
+//        return new RestHighLevelClient(builder);
+//
+//    }
 
     /**
      * 高版本客户端
